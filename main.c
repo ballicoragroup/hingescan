f2dd77e8949942c7bab0bf31f6c7ba8416b72e95
Miguel A. Ballicora <mballic@luc.edu>
2012-12-23 10:04:10 -0600

    [v0.0.3] upload array of pdb and calculate rmsd, ref = 0

--- main.c
+++ main.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include "bool.h"
 #include "pdb.h"
 #include "fit.h"
 #include <math.h>
+
+#define MAXCOOR 10000
+struct coordinates Coor[MAXCOOR];
+int N_coor = 0;
 
 void mod2_CAcoord(const struct model *m, struct coordinates *c);
 void mod2_ALLcoord(const struct model *m, struct coordinates *c, int from, int to);
@@ -14,6 +19,7 @@ struct model model [2];
 #define ATOM_FROM  1
 #define ATOM_TO   43
 
+char *folder_name;
 
 int main(int argc, char *argv[])
 {
@@ -26,15 +32,76 @@ int main(int argc, char *argv[])
  	struct coordinates *pma;
 	struct coordinates *pmb;
 
-    if (argc < 4) {
+    if (argc < 2) {
     	printf("Not enough parameters\n");
     	printf("Usage: %s [pdbfile1] [pdbfile2]\n", "rmsd");
         exit(EXIT_FAILURE);	
     } 
     
  	namei = argv[1];
- 	nameb = argv[2];
- 	nameo = argv[3];  
+//	nameb = argv[2];
+// 	nameo = argv[3];  
+
+folder_name = "/home/miguel/Dropbox/arg32/md/ATP_frames_ALA/";
+
+
+{
+	char *name_source = argv[1];
+	FILE *fs;
+	char name_line[1024];
+	char name_i[1024];
+	struct model model_input;
+
+	if (NULL != (fs = fopen(name_source, "r"))) {
+
+printf ("open: %s\n",name_source);
+
+		
+
+
+
+		while (fgets(name_line, 1024, fs)) {
+			char *j;
+			//FIXME trim  blanks and spaces
+			for (j = name_line; *j && isspace(*j); j++) {;}
+			if (*j == '\0') continue;
+
+			name_line[strlen(name_line)-1] = '\0';
+
+			name_i[0] = '\0';
+			strcpy(name_i, folder_name);
+			strcat(name_i, name_line);
+
+			if (NULL != (fi = fopen(name_i, "r"))) {
+				struct coordinates *pma = &Coor[N_coor++];
+
+				printf ("read: %s\n",name_i);
+
+				modelload(fi, &model_input);
+				mod2_ALLcoord (&model_input, pma, ATOM_FROM, ATOM_TO);
+				fclose(fi);
+			} else {
+				printf("problems with %s\n", namei);
+			}
+		}
+		fclose(fs);	
+	} else {
+		printf("problems with %s\n", name_source);
+	}
+
+	printf ("Files read=%d\n", N_coor);
+}
+
+{
+	int i;
+	struct transrot tr;
+	for (i = 1; i < N_coor; i++) {
+		rmsd = fit (&Coor[0], &Coor[i], &tr);
+		printf("RMSD [%d,%d]: %.4lf\n",0,i,rmsd);
+	}
+
+	exit(0);
+}
 
 	if (NULL != (fi = fopen(namei, "r"))) {
  		if (NULL != (fb = fopen(nameb, "r"))) {
@@ -77,7 +144,7 @@ int main(int argc, char *argv[])
 		printf("problems with %s\n", namei);
     }
 
-    return 0;
+    return EXIT_SUCCESS;
 }
 
 
